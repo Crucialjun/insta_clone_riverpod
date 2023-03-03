d51c1e418d043eefdbe91e44d088d6402c2f8421
Crucialjun <nich.otieno@gmail.com>
2023-03-04 01:17:53 +0300

    auth

---
+++ lib/state/auth/constants/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class Constants {
+  static const accountExistsWithDifferentCredentials =
+      'account-exists-with-different-credentials';
+  static const googleCom = 'google.com';
+  static const emailScope = 'email';
+
+  const Constants._();
+}
