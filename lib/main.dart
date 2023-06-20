24bb76b8afe3c965d474d724c6d288f68deaac29
Nicholas Otieno <nich.otieno@gmail.com>
2023-06-20 20:21:24 +0300

    auth done

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:insta_clon_rivrpo/firebase_options.dart';
-import 'package:insta_clon_rivrpo/state/auth/backend/authenticator.dart';
 import 'package:insta_clon_rivrpo/state/auth/models/auth_results.dart';
 import 'package:insta_clon_rivrpo/state/auth/providers/auth_state_provider.dart';
 
@@ -42,30 +41,26 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LoginView extends StatelessWidget {
+class LoginView extends ConsumerWidget {
   const LoginView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         TextButton(
-          onPressed: () {
-            Authenticator().logout();
-          },
+          onPressed: () {},
           child: const Text("Log Out"),
         ),
         TextButton(
           onPressed: () async {
-            final result = await Authenticator().loginWithGoogle();
-            print(result.toString());
+            ref.read(authStateProvider.notifier).loginWithGoogle();
           },
           child: const Text("Sign In WIth google"),
         ),
         TextButton(
           onPressed: () async {
-            final result = await Authenticator().loginWithFacebook();
-            print(result.toString());
+            ref.read(authStateProvider.notifier).loginWithFacebook();
           },
           child: const Text("Sign In WIth Facebook"),
         ),
